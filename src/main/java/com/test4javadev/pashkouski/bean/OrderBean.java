3b26ddf5834ade7553766e27b3716649ef8fdf40
lllbllllb <enakes@gmail.com>
2016-10-23 01:41:48 +0300

    Context not created

--- src/main/java/com/test4javadev/pashkouski/bean/OrderBean.java
+++ src/main/java/com/test4javadev/pashkouski/bean/OrderBean.java
@@ -1,10 +1,8 @@
 package com.test4javadev.pashkouski.bean;
 
 import com.test4javadev.pashkouski.dao.OrderDao;
-import com.test4javadev.pashkouski.dao.OrderDaoImpl;
 import com.test4javadev.pashkouski.model.Order;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
@@ -16,17 +14,21 @@ public class OrderBean implements Serializable {
 
     private static final long serialVersionUID = 109419129003495251L;
 
-    private OrderDao orderDAO = new OrderDaoImpl();
+
+    Order order;
+
+    private OrderDao orderDAO = new OrderDao();
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 
     public List<Order> getAll() {
-        System.out.println("!!!!!!!!!!!!!!!!!!!!");
         return orderDAO.getAll();
     }
 
     public Order getOrder() {
-        Order order = orderDAO.get();
-        System.out.println("!!!!!!!!!!!!!!!!!!!! " + order);
-        return order;
+        return orderDAO.get();
     }
 
 //    @PostConstruct
--- src/main/java/com/test4javadev/pashkouski/dao/OrderDao.java
+++ src/main/java/com/test4javadev/pashkouski/dao/OrderDao.java
@@ -2,9 +2,25 @@ package com.test4javadev.pashkouski.dao;
 
 import com.test4javadev.pashkouski.model.Order;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
-public interface OrderDao {
-    List<Order> getAll();
-    Order get();
+@Stateless
+public class OrderDao {
+
+    @PersistenceContext(unitName = "vieworders")
+    private EntityManager em;
+
+    public List<Order> getAll() {
+        Query query = em.createQuery("SELECT o FROM Order as o");
+        List<Order> orders = query.getResultList();
+        return orders;
+    }
+
+    public Order get() {
+        return getAll().get(0);
+    }
 }
